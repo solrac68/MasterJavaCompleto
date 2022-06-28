d70bcfad146531913933f0a4b5516c087f71fed8
solrac68 <solrac.hincapie@gmail.com>
2022-06-27 19:47:01 -0500

    System.properties personalizadas

---
+++ HolaMundo/src/AsignarPropiedadesDeSistema.java
@@ -0,0 +1,24 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
+public class AsignarPropiedadesDeSistema {
+    public static void main(String[] args) {
+        try {
+            FileInputStream archivo = new FileInputStream("src/config.properties");
+            Properties p = new Properties(System.getProperties()); //Se cargan todas las configuraciones del sistema en el objeto properties
+            p.load(archivo);
+            p.setProperty("mi.propiedad.personalizada","mi valor guardado");
+            System.setProperties(p);
+            Properties ps = System.getProperties();
+            String otra = ps.getProperty("otra");
+            ps.list(System.out);
+            System.out.println("otra = " + otra);
+            String nombre = System.getProperty("config.autor.nombre");
+            System.out.println("nombre = " + nombre);
+        } catch (Exception e) {
+            System.out.println("NO existe el archivo = " + e);
+        }
+
+    }
+}
