00247acc744fe7ae1407eae10b3334ec59533510
solrac68 <solrac.hincapie@gmail.com>
2022-07-09 17:57:11 -0500

    Random

--- HolaMundo/src/EjemploClassMathRandom.java
+++ HolaMundo/src/EjemploClassMathRandom.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class EjemploClassMathRandom {
     public static void main(String[] args) {
         String[] colores = {"azul","amarillo","rojo","verde","blanco","negro"};
@@ -7,5 +9,17 @@ public class EjemploClassMathRandom {
         System.out.println("random = " + random);
         random = Math.floor(random);
         System.out.println("color = " + colores[(int)random]);
+
+        Random random1 = new Random();
+        int randomInt = random1.nextInt(7);
+        System.out.println("randomInt = " + randomInt);
+
+        int random2 = 7 + random1.nextInt(9); // Aleatorio entre 7 y 15
+        System.out.println("random2 = " + random2);
+
+        int random3 = random1.nextInt(colores.length); // Aleatorio entre 0 y colores.length
+        System.out.println("color = " + colores[random3]); // Imprime los colores
+        
+
     }
 }
